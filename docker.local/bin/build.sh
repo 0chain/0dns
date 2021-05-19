a0e82433b1f6a0519699d8d5ae505523c3cd136f
Lz <imlangzi@qq.com>
2021-05-19 15:08:21 +0800

    fix(shell): expose VOLUMES_MONGO_DATA,VOLUMES_CONFIG and VOLUMES_LOG in docker-compose.yml

--- docker.local/bin/build.sh
+++ docker.local/bin/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker-compose -p 0dns -f docker.local/docker-compose.yml build --force-rm
+#mount volumns
+
+VOLUMES_CONFIG=./config VOLUMES_LOG=./0dns/log VOLUMES_MONGO_DATA=./0dns/mongodata docker-compose -p 0dns -f docker.local/docker-compose.yml build --force-rm
 docker.local/bin/sync_clock.sh
