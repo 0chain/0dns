f2b77855d91d1350edd730a1ecf61e26947f5f49
Andika Demas Riyandi <andika.riyan@gmail.com>
2021-12-21 19:01:33 +0700

    change docker path from root folder (#22)
    * change docker path from root folder
    
    * update README

--- docker.local/bin/build.sh
+++ docker.local/bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker-compose -p 0dns -f docker.local/docker-compose.yml build --force-rm
+docker-compose -p 0dns -f ./docker.local/docker-compose.yml build --force-rm
 docker.local/bin/sync_clock.sh
